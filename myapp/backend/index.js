35100d0374660e4a2511bffe0ac2641aa46428c1
User <user@example.com>
2026-02-15 12:19:25 +0530

    Fix render.yaml (staticSites) and backend PORT

--- myapp/backend/index.js
+++ myapp/backend/index.js
@@ -549,7 +549,7 @@ app.post("/books/download/:bid/:uid", (req, res) => {
   });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
